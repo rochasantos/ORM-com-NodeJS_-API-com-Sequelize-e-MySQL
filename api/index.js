0ea88d9702273a9efd1e7eb50a763b8d98ff56b6
Adriano Santos <adriano_angelo@live.com>
2022-08-30 20:39:10 -0300

    Atividade 3.5 - Organizando as rotas

--- api/index.js
+++ api/index.js
@@ -1,14 +1,10 @@
 const express = require('express')
+const routes = require('./routes')
 
 const app = express()
-
 const port = 3000
 
-app.use(express.json())
-
-app.get('/teste', (req, res) => res
-  .status(200)
-  .send({ mensagem: 'boas-vindas à API'}))
+routes(app);
 
 app.listen(port, () => console.log(`servidor rodando na porta ${port}`))
 

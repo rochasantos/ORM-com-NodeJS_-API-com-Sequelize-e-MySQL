bf08f1cc0074058e7f39f54155a517e30e02f855
Adriano Santos <adriano_angelo@live.com>
2022-08-30 21:09:59 -0300

    Atividade 3.7 - Definindo a primeira rota

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -3,7 +3,7 @@ const database = require('../models')
 class PessoaController {
   static async pegaTodasAsPessoas(req, res) {
     try {
-      const todasAsPessoas = await database.Pessoa.findAll()
+      const todasAsPessoas = await database.Pessoas.findAll()
       return res.status(200).json(todasAsPessoas)
     } catch (error) {
       return res.status(500).json(error.message)
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const pessoas = require('./pessoasRoute.js')
 
 module.exports = app => {
   app.use(express.json())
+  app.use(pessoas)
   app.get('/', (req, res) => res.send('Olá!'))
 }
